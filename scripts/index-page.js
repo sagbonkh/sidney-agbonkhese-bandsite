932851dba00149288942ab81290a635e3505c3ef
sagbonkh <sidneyagbonkhese9@gmail.com>
2024-02-22 23:16:49 -0800

    created template html for dynamic comments

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,12 +1,12 @@
-let comments_sect = document.querySelector(".comments");
+let commentSect = document.querySelector(".comments");
 let title = document.createElement("h2");
 title.classList.add("comments-title");
 title.textContent = `Join The Conversation`;
-comments_sect.appendChild(title);
+commentSect.appendChild(title);
 
 let form = document.createElement("form");
 form.classList.add("comments-form");
-comments_sect.appendChild(form);
+commentSect.appendChild(form);
 
 let avatar = document.createElement("img");
 avatar.src = "../assets/images/Mohan-muruge.jpg";
@@ -14,17 +14,55 @@ avatar.alt = "avatar";
 avatar.classList.add("comments-avatar");
 form.appendChild(avatar);
 
-let input_area = document.createElement("section");
-input_area.classList.add("comments-input");
-form.appendChild(input_area);
+let inputArea = document.createElement("section");
+inputArea.classList.add("comments-input");
+form.appendChild(inputArea);
 
-let name_label = document.createElement("label");
-name_label.classList.add("comments-text");
-name_label.htmlFor = "name";
-name_label.textContent = `NAME`;
-input_area.appendChild(name_label);
+let nameLabel = document.createElement("label");
+nameLabel.classList.add("comments-text");
+nameLabel.htmlFor = "name";
+nameLabel.textContent = `NAME`;
+inputArea.appendChild(nameLabel);
 
-let name_input = document.createElement("input");
-name_input.setAttribute("type","text");
-name_input.placeholder= "Enter your name";
-input_area.appendChild(name_input);
+let nameInput = document.createElement("input");
+nameInput.setAttribute("type", "text");
+nameInput.placeholder = "Enter your name";
+nameInput.name = "name";
+inputArea.appendChild(nameInput);
+
+let commentLabel = document.createElement("label");
+commentLabel.classList.add("comments-text");
+commentLabel.htmlFor = "comment";
+commentLabel.textContent = `COMMENT`;
+inputArea.appendChild(commentLabel);
+
+let textArea = document.createElement("textarea");
+textArea.setAttribute("type", "text");
+textArea.placeholder = "Add a new comment";
+textArea.name = "comment";
+inputArea.appendChild(textArea);
+
+let submitBtn = document.createElement("button");
+submitBtn.textContent = "COMMENT";
+submitBtn.setAttribute("type", "submit");
+inputArea.appendChild(submitBtn);
+
+const txtA = `This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic workdeserves reverence. Let us appreciate this for what it is and what it contains.`;
+const txtB = `I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.`;
+const txtC = `I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.`;
+let starterObjectA = {
+    name:'Victor Pinto',
+    comment:txtA,
+    date:'11/02/2023'
+};
+let starterObjectB = {
+    name:'Christina Cabrera',
+    comment:txtB,
+    date:'10/28/2023'
+};
+let starterObjectC = {
+    name:'Isaac Tadesse',
+    comment:txtC,
+    date:'10/20/2023'
+};
+let commentsArray = [starterObjectA, starterObjectB, starterObjectC];
